d08a5db321b49ddf24ee8a02ca8d76ea24736d9a
ibrahimfe <ibrahiimm.f@gmail.com>
2023-05-31 17:16:00 +0700

    added BFS and DFS algorithm

--- output/graph_2.cpp
+++ output/graph_2.cpp
@@ -1,88 +1,131 @@
 #include <iostream>
-#include <vector>
-#include <queue>
 #include <list>
+#include <queue>
 
 using namespace std;
 
 class Graph
 {
-  int numNodes;
-  list<int> *adjList;
+	int numNodes;
+	list<int> *adjList;
 
 public:
-  Graph(int numNodes)
-  {
-    this->numNodes = numNodes;
-    adjList = new list<int>[numNodes];
-  }
+	Graph(int numNodes)
+	{
+		this->numNodes = numNodes;
+		adjList = new list<int>[numNodes];
+	}
 
-  void addEdge(int node1, int node2)
-  {
-    adjList[node1].push_back(node2);
-    adjList[node2].push_back(node1);
-  }
-  void printGraph()
-  {
-    for (int i = 0; i < numNodes; ++i)
-    {
-      cout << "Node " << i << ": ";
-      for (int node : adjList[i])
-      {
-        cout << node << " ";
-      }
-      cout << endl;
-    }
-  }
-  bool isPathExists(int startNode, int endNode)
-  {
-    vector<bool> visited(numNodes, false);
-    queue<int> q;
-    visited[startNode] = true;
-    q.push(startNode);
+	void addEdge(int node1, int node2)
+	{
+		adjList[node1].push_back(node2);
+		adjList[node2].push_back(node1);
+	}
+	void printGraph()
+	{
+		for (int i = 0; i < numNodes; ++i)
+		{
+			cout << "Node " << i << ": ";
+			for (int node : adjList[i])
+			{
+				cout << node << " ";
+			}
+			cout << endl;
+		}
+	}
 
-    while (!q.empty())
-    {
-      int currentNode = q.front();
-      q.pop();
+	void BFS(int startNode)
+	{
+		bool *visited = new bool[numNodes];
+		for (int i = 0; i < numNodes; i++)
+		{
+			visited[i] = false;
+		}
+		queue<int> q;
+		visited[startNode] = true;
+		q.push(startNode);
 
-      if (currentNode == endNode)
-        return true;
+		while (!q.empty())
+		{
+			int currentNode = q.front();
+			cout << currentNode << " ";
+			q.pop();
 
-      for (int adjNode : adjList[currentNode])
-      {
-        if (!visited[adjNode])
-        {
-          visited[adjNode] = true;
-          q.push(adjNode);
-        }
-      }
-    }
+			for (int neighbor : adjList[currentNode])
+			{
+				if (!visited[neighbor])
+				{
+					visited[neighbor] = true;
+					q.push(neighbor);
+				}
+			}
+		}
+		delete[] visited;
+	}
 
-    return false;
-  }
+	void DFSUtil(int currentNode, bool visited[])
+	{
+		visited[currentNode] = true;
+		cout << currentNode << " ";
+		for (int neighbor : adjList[currentNode])
+		{
+			if (!visited[neighbor])
+			{
+				DFSUtil(neighbor, visited);
+			}
+		}
+	}
+
+	void DFS(int startNode)
+	{
+		bool *visited = new bool[numNodes];
+		for (int i = 0; i < numNodes; ++i)
+		{
+			visited[i] = false;
+		}
+		DFSUtil(startNode, visited);
+		delete[] visited;
+	}
 };
+
+void menu()
+{
+	cout << "1. Display graph" << endl;
+	cout << "2. Implementasi BFS" << endl;
+	cout << "3. Implementasi DFS" << endl;
+	cout << "4. Exit" << endl;
+	cout << "=============================" << endl;
+	cout << "Masukkan pilihan anda : ";
+}
 
 int main(int argc, char const *argv[])
 {
-  Graph graph(5);
-  graph.addEdge(0, 1);
-  graph.addEdge(0, 4);
-  graph.addEdge(1, 2);
-  graph.addEdge(1, 3);
-  graph.addEdge(1, 4);
-  graph.addEdge(2, 3);
-  graph.addEdge(3, 4);
-  graph.printGraph();
+	Graph graph(5);
+	graph.addEdge(0, 1);
+	graph.addEdge(0, 4);
+	graph.addEdge(1, 2);
+	graph.addEdge(1, 3);
+	graph.addEdge(1, 4);
+	graph.addEdge(2, 3);
+	graph.addEdge(3, 4);
 
-  if (graph.isPathExists(0, 4))
-  {
-    cout << "There is a path between the node 0 and 4" << endl;
-  }
-  else
-  {
-    cout << "There is no path between the node" << endl;
-  }
+	string choice;
+	menu();
+	cin >> choice;
+	if (choice == "1")
+	{
+		graph.printGraph();
+	}
+	else if (choice == "2")
+	{
+		cout << "BFS :" << endl;
+		graph.BFS(1);
+	}
+	else if (choice == "3")
+	{
+		cout << "DFS :" << endl;
+		graph.DFS(1);
+	}
 
-  return 0;
+	return 0;
 }
